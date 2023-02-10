27d273debf9c1b57bbb56f5c81c33dfe62558737
Robin SHAMSNEJAD <rsham@free.fr>
2023-02-10 16:14:49 +0100

    Add TD1 contents

--- TD1/TD1.py
+++ TD1/TD1.py
@@ -54,22 +54,53 @@ for i in categories_values.keys():
 Xnum.boxplot()
 plt.ylabel("Valeurs")
 plt.title("Répartition des différentes valeurs")
+
+######## HISTOGRAMMES POUR QUANTITA, BARGRAPH FOR QUALITA ######################
 # Histogram of quantitative variables
 Xnum[['Height', 'Weight']].hist()
-plt.ylabel("Quantité absolue")
+ax1 = plt.subplot(1,2,1)
+ax1.set_xlabel("Taille (cm)")
+ax1.set_ylabel("Quantité absolue")
+ax2 = plt.subplot(1,2,2)
+ax2.set_xlabel("Poids (kg)")
+ax2.set_ylabel("Quantité absolue")
 plt.suptitle("Répartition des tailles et poids")
+
 Xnum[['Pulse1', 'Pulse2']].hist()
-plt.ylabel("Quantité absolue")
+ax1 = plt.subplot(1,2,1)
+ax1.set_xlabel("Pouls (bpm)")
+ax1.set_ylabel("Quantité absolue")
+ax2 = plt.subplot(1,2,2)
+ax2.set_xlabel("Pouls (bpm)")
+ax2.set_ylabel("Quantité absolue")
 plt.suptitle("Répartition des pouls relevés")
 
+###### NP.UNIQUE PERMET DE FAIRE APPARAITRES LES ZÉROS #########################
 plt.figure()
 ax1 = plt.subplot(1, 2, 1)
-Xnum['Age'].value_counts().plot(kind='bar', ax=ax1)
+#Xnum['Age'].value_counts().plot(kind='bar', ax=ax1)
+values, counts = np.unique(Xnum.iloc[:, 2], return_counts=True)
+ax1.bar(values, counts, width=0.5)
 ax1.set_ylabel("Quantité absolue")
 ax2 = plt.subplot(1, 2, 2)
 Xnum['Year'].value_counts().plot(kind='bar', ax=ax2)
 ax2.set_ylabel("Quantité absolue")
 plt.suptitle("Répartition des âges et promotions")
 
-plt.show()
+#plt.show()
 
+print("Modalités pour Xcat :\n", Xcat.value_counts())
+
+Xnum.plot.scatter(x='Height', y='Weight')
+pd.plotting.scatter_matrix(Xnum)
+
+pprint(Xnum.corr())
+
+crosstab = pd.crosstab(X['Gender'], X['Smokes'])
+crosstab.plot(kind='bar', stacked=True)
+
+pprint(X.groupby("Gender").mean())
+
+X.boxplot(column=['Height'], by=['Gender'])
+
+plt.show()
