3a187bc73e87bdc339a1f5137e0fbff559319020
Robin SHAMSNEJAD <rsham@free.fr>
2023-03-14 22:35:00 +0100

    Add stuff

--- TD2/partie2a.py
+++ TD2/partie2a.py
@@ -6,110 +6,70 @@ from pprint import pprint
 import seaborn
 import numpy as np
 
-# Get the absolute path to the dataset that is alongside the Python script
-scriptdir = op.dirname(op.abspath(__file__))
-datafile  = op.join(scriptdir, "Criminalite.xlsx")
+#!/usr/bin/python
 
-# On charge les données
-X = pd.read_excel(
-    datafile,
-    sheet_name=0,
-    header=0,
-    index_col=0
+#%%
+# Import modules
+import sys
+import os.path as op
+libdir = op.join(op.dirname(op.dirname(op.abspath(__file__))), "TDlib")
+sys.path.append(libdir)
+import TDlib as td
+
+#%%
+# Import data
+
+X = td.excelToPandas(
+    "Criminalite.xlsx",
+    0,
+    0,
+    0,
+    []
 )
 
-n = X.shape[0]
-p = X.shape[1]
+#%%
+## 1. Observer très rapidement ces données et regarder si des états se différencient des autres etc.
+td.printMeanAndVar(X)
+td.displayBoxplot(X)
 
 #%%
-## 1
-pprint(X.mean())
-pprint(X.var())
-X.boxplot()
-plt.ylabel("Notes sur 20")
-plt.title("Répartition des notes")
+## 2. Donner les moyennes et les écarts types des différentes variables. Donnez également la variance totale du nuage de points.
+
+td.displayCorrelationMatrix(X, "Correlation matrix of the data")
+td.displayScatterMatrix(X, "Scatter matrix of the data")
 
 #%%
-## 2
-seaborn.heatmap(X.corr(), cmap="coolwarm", annot=True)
-plt.show()
-pd.plotting.scatter_matrix(X)
-plt.title("Nuages de points 2 à 2")
-plt.show()
+## 3. Regarder les distributions des différentes variables.
+td.displayCovarianceMatrix(X, "Covariance matrix of the data")
 
 #%%
-## 3
-print(X.cov())
-
-#%%
-## 4
+## 4. Calculer les corrélations entre les variables.
 from sklearn.decomposition import PCA
 acp = PCA()
 Xacp = acp.fit(X).transform(X)
 print(Xacp)
 
 #%%
-## 5
-y = list(acp.explained_variance_ratio_)
-x = range(len(y))
-ycum = np.cumsum(y)
-plt.bar(x,y)
-plt.plot(x,ycum,"-r")
-plt.xlabel("Nombre de facteurs (ou vecteurs propres)")
-plt.ylabel("Variances expliquées et cumul de variance expliquée")
-plt.title("Diagramme de Pareto")
+## 5. Utiliser une représentation graphique pour visualiser les coefficients de corrélations
 
-plt.show()
+td.displayParetoDiagram(X, "Pareto graph for the data")
 
 # Ici on voit qu'avex deux composantes principales, on s'approche suffisamment
 # des 100% de la variance cumulée donc elles suffiront pour décrire
-# %%
-## 6
 
-print(acp.explained_variance_.sum())
-print(X.var().sum())
+# %%
+## 6. Commentez les résultats
+
+print(td.totalVariance(X))
 
 # %%
 ## 7
-plt.scatter(Xacp[:, 0], Xacp[:, 1])
-for i, label in enumerate(X.index):
-    plt.annotate(label, (Xacp[i,0], Xacp[i,1]))
 
-plt.xlabel("Composante principale 1")
-plt.ylabel("Composante principale 2")
-plt.title("Individus dans le plan des CP1 et CP2")
-plt.show()
+td.displayPopulationInFirstMainComponents(X)
 
 # %%
 ## 8
 
-# corvar est de dimension (n,2) : contient dans la colonne 0 : la corrélation entre la composante principale 1 et les variables de départ 
-# et dans la colonne 1 la corrélation entre la composante principale 2 et les variables de départ
+td.displayCorrelationCircle(X)
 
-corvar = np.zeros((p,2))
-
-for i in range(p):
-    for j in range(2):
-        corvar[i,j] = np.corrcoef(X.iloc[:,i], Xacp[:,j])[0,1]
-
-# Cercle des corrélations
-fig, axes = plt.subplots(figsize=(8,8))
-axes.set_xlim(-1,1)
-axes.set_ylim(-1,1)
-
-# On ajoute les axes
-plt.plot([-1,1],[0,0],color='silver',linestyle='-',linewidth=1)
-plt.plot([0,0],[-1,1],color='silver',linestyle='-',linewidth=1)
-# On ajoute un cercle
-cercle = plt.Circle((0,0),1,color='blue',fill=False)
-axes.add_artist(cercle)
-plt.xlabel("Composante principale 1")
-plt.ylabel("Composante principale 2")
-plt.title('Cercle des corrélations')
-plt.scatter(corvar[:,0],corvar[:,1])
-#affichage des étiquettes (noms des variables)
-for j in range(p):
-  plt.annotate(X.columns[j],(corvar[j,0],corvar[j,1]))
-
-plt.show()
 # %%
