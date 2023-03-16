80db9b92eaa9c9413863fd75ba5e27f738b485ad
Robin SHAMSNEJAD <robin.shamsnejad@arturia.com>
2023-03-16 13:11:32 +0100

    Fix intellisense for TDlib

--- TD2/partie1.py
+++ TD2/partie1.py
@@ -4,7 +4,7 @@
 # Import modules
 import sys
 import os.path as op
-libdir = op.join(op.dirname(op.dirname(op.abspath(__file__))), "TDlib")
+libdir = op.dirname(op.dirname(op.abspath(__file__)))
 sys.path.append(libdir)
 import TDlib as td
 
